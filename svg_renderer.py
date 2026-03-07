a448bc32ef4b1e3b46c696def84ccf1c4a4b98bb
angelwhomst <angelolivarez810@gmail.com>
2026-03-07 17:19:52 +0800

    refactor: streamline svg data handling in svg_renderer.py

--- svg_renderer.py
+++ svg_renderer.py
@@ -4,30 +4,26 @@ def svg_overwrite(filename, age_data, commit_data, star_data, repo_data, contrib
     tree = etree.parse(filename)
     root = tree.getroot()
     
-    justify_format(root, 'age_data', age_data)
-    justify_format(root, 'commit_data', commit_data)
-    justify_format(root, 'star_data', star_data)
-    justify_format(root, 'repo_data', repo_data)
-    justify_format(root, 'contrib_data', contrib_data)
-    justify_format(root, 'loc_data', loc_data[2])
-    justify_format(root, 'loc_add', loc_data[0])
-    justify_format(root, 'loc_del', loc_data[1])
+    justify_format(root, 'age_data', age_data, 29) 
+    
+    find_and_replace(root, 'repo_data', f"{repo_data:,}")
+    find_and_replace(root, 'contrib_data', f"{contrib_data:,}")
+    find_and_replace(root, 'star_data', f"{star_data:,}")
+    find_and_replace(root, 'commit_data', f"{commit_data:,}")
+    find_and_replace(root, 'loc_data', f"{loc_data[2]}")
+    find_and_replace(root, 'loc_add', f"{loc_data[0]}")
+    find_and_replace(root, 'loc_del', f"{loc_data[1]}")
     
     tree.write(filename, encoding='utf-8', xml_declaration=True)
 
 def justify_format(root, element_id, new_text, length=0):
-    if isinstance(new_text, int):
-        new_text = f"{'{:,}'.format(new_text)}"
     new_text = str(new_text)
     find_and_replace(root, element_id, new_text)
     
-    just_len = max(0, length - len(new_text))
-    if just_len <= 2:
-        dot_map = {0: '', 1: ' ', 2: '. '}
-        dot_string = dot_map[just_len]
-    else:
-        dot_string = ' ' + ('.' * just_len) + ' '
-    find_and_replace(root, f"{element_id}_dots", dot_string)
+    if length > 0:
+        just_len = max(0, length - len(new_text))
+        dot_string = " ." + ("." * just_len) + " "
+        find_and_replace(root, f"{element_id}_dots", dot_string)
 
 def find_and_replace(root, element_id, new_text):
     element = root.find(f".//*[@id='{element_id}']")
